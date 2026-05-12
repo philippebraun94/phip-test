b770bb224500fe4dfcfad552225421c139116c45
philippebraun94 <philippe.braun94@gmail.com>
2026-05-12 22:01:19 +0200

    adjust

--- app/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://cynqzzlermzoiilzwdac.supabase.co'
-const supabaseAnonKey = 'sb_publishable_2i-AFjEahFdzaGhptaHvRQ_Jm8LnSkX'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
